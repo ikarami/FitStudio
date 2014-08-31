f4676f090356a4d4ca51d71c058732a8c95d1e9f
Przemyslaw Banka <pbanka@antennasoftware.com>
2014-08-31 11:38:25 +0200

    Adding local copy of boostrap css, fonts and js

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,20 +1,20 @@
 require.config({
     paths: {
         jquery: 'libs/jquery-2.1.1',
+        bootstrap: 'libs/bootstrap.min',
         knockout: 'libs/knockout-3.1.0',
         'ko.customBindings': 'libs/knockout.customBindings',
-        'kb': 'libs/knockback',
+        kb: 'libs/knockback',
         underscore: 'libs/underscore',
         backbone: 'libs/backbone',
         text: 'libs/text',
         locale: 'modules/locale',
         templates: '../templates',
-        i18n: '../i18n',
-        bootstrap: 'http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min'
+        i18n: '../i18n'
     },
 
     shim: {
-        'kb': ['knockout'],
+        kb: ['knockout'],
         'ko.customBindings': ['knockout'],
         backbone: ['underscore', 'jquery'],
         FitStudio: ['backbone'],
