63dfbb62ce9f654f897adf90e561efcfea824eb6
Przemyslaw Banka <przemyslaw.banka@ubnt.com>
2015-06-26 23:18:08 +0200

    Adding findBetweenDates to entry model and preparing entries/:courseId route to accept start/end params

--- app/models/Entry.js
+++ app/models/Entry.js
@@ -1,7 +1,13 @@
 module.exports = function (logger, mongoose) {
     'use strict';
 
-    var EntrySchema, Entry, findAll, findById, add, edit;
+    var EntrySchema, Entry, findAll, findById, findBetweenDates, add, edit;
+
+    var _ = require('lodash-node');
+
+    var timeStampToDate = function (timestamp) {
+        return new Date(Number(timestamp)).toString();
+    };
 
     EntrySchema = new mongoose.Schema({
         accountId: {type: mongoose.Schema.Types.ObjectId},
@@ -16,16 +22,55 @@ module.exports = function (logger, mongoose) {
 
     Entry = mongoose.model('Entry', EntrySchema);
 
+    findBetweenDates = function (ids, dates, callback) {
+        var query;
+        logger.log('Entry :: findBetweenDates :: ' + ids.accountId + ' courseId: ' + ids.courseId);
+        query = {
+            accountId: ids.accountId,
+            courseId: ids.courseId
+        };
+        if (query.courseId === null) {
+            delete query.courseId;
+        }
+
+        // check for timestamps -> dates have to be string objects here!
+        if (!_.isNaN(Number(dates.startDate))) {
+            logger.log('gotya');
+            dates.startDate = timeStampToDate(dates.startDate);
+        }
+        if (!_.isNaN(Number(dates.endDate))) {
+            logger.log('gotya');
+            dates.endDate = timeStampToDate(dates.endDate);
+        }
+
+        logger.log('Entry :: findBetweenDates :: startDate ' + dates.startDate + ' endDate: ' + dates.endDate);
+        query.startDate = {
+            $gte: new Date(dates.startDate),
+            $lt: new Date(dates.endDate)
+        };
+        Entry.find(query, function (err, docs) {
+            callback(docs);
+        });
+    };
+
     findById = function (ids, callback) {
-        logger.log('Entry :: findById :: ' + ids.accountId);
+        logger.log('Entry :: findById :: ' + ids.accountId + ' courseId: ' + ids.courseId + ' entryId: ' + ids.entryId);
         Entry.findOne({accountId: ids.accountId, courseId: ids.courseId, _id: ids.entryId}, function (err, docs) {
             callback(docs || false);
         });
     };
 
     findAll = function (ids, callback) {
+        var query;
         logger.log('Entry :: findAll :: ' + ids.accountId);
-        Entry.find({accountId: ids.accountId, courseId: ids.courseId}, function (err, docs) {
+        query = {
+            accountId: ids.accountId,
+            courseId: ids.courseId
+        };
+        if (query.courseId === null) {
+            delete query.courseId;
+        }
+        Entry.find(query, function (err, docs) {
             callback(docs);
         });
     };
@@ -35,8 +80,8 @@ module.exports = function (logger, mongoose) {
         var course = new Entry({
             accountId: data.accountId,
             courseId: data.courseId,
-            startDate: data.startDate,
-            endDate: data.endDate,
+            startDate: new Date(data.startDate),
+            endDate: new Date(data.endDate),
             comments: data.comments || '',
             missedUsers: data.missedUsers || [],
             actualUsers: data.actualUsers || [],
@@ -69,9 +114,10 @@ module.exports = function (logger, mongoose) {
     };
 
     return {
-        findAll: findAll,
         add: add,
-        findById: findById,
-        edit: edit
+        edit: edit,
+        findAll: findAll,
+        findBetweenDates: findBetweenDates,
+        findById: findById
     };
 };
--- app/routes/entries.js
+++ app/routes/entries.js
@@ -3,17 +3,34 @@ module.exports = function (app, models) {
 
     app.get('/entries/:courseId/', [app.authChecker], function (req, res) {
         app.logger.log('Entries route :: getting entries for course id: ' + req.params.courseId);
-        var accountId = req.session.accountId;
+        var queryParams,
+            ids,
+            accountId = req.session.accountId;
 
         if (req.params.courseId === 'me') {
             req.params.courseId = null;
         }
 
-        // todo search req.params for string query parameters
+        ids = {accountId: accountId, courseId: req.params.courseId};
 
-        models.Entry.findAll({accountId: accountId, courseId: req.params.courseId}, function (instructors) {
-            res.send(instructors);
-        });
+        if (Object.keys(req.query).length === 0) {
+            models.Entry.findAll(ids, function (entries) {
+                res.send(entries);
+            });
+        } else {
+            app.logger.log('Entries route :: query params :: ' + JSON.stringify(req.query));
+            queryParams = req.query;
+            if (queryParams.startDate && queryParams.endDate) {
+                models.Entry.findBetweenDates(ids, {
+                    startDate: queryParams.startDate,
+                    endDate: queryParams.endDate
+                }, function (entries) {
+                    res.send(entries);
+                });
+            } else {
+                res.status(204).end();
+            }
+        }
     });
 
     app.post('/entries/:courseId/:entryId', [app.authChecker], function (req, res) {
