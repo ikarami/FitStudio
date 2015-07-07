8bcfff8534d87f2f90ecb5a1fac45e42a9077194
Przemyslaw Banka <przemyslaw.banka@ubnt.com>
2015-07-07 22:29:31 +0200

    Entry details - adding users which are not assigned the course to list of actual users

--- app/public/js/views/entryDetails.js
+++ app/public/js/views/entryDetails.js
@@ -4,8 +4,9 @@ define(['jquery',
     'knockout',
     'kb',
     'collections/courses',
+    'collections/users',
     'models/entry',
-    'text!templates/entryDetails.html'], function ($, _, Backbone, ko, kb, coursesCollection, EntryModel, entryDetailsTemplate) {
+    'text!templates/entryDetails.html'], function ($, _, Backbone, ko, kb, coursesCollection, usersCollection, EntryModel, entryDetailsTemplate) {
     'use strict';
 
     var EntryDetailsView = Backbone.View.extend({
@@ -13,6 +14,20 @@ define(['jquery',
 
         initialize: function (args) {
             var view = this, ViewModel;
+
+            view.on('save', function (args) {
+                if (args.field === 'courseUsers') {
+                    if (args.selected && args.selected.length) {
+                        args.selected.forEach(function (id) {
+                            var model = usersCollection.findWhere({_id: id});
+                            if (model) {
+                                view.viewModel.markAsPresent(model.getShortInfo()) ;
+                            }
+                        });
+
+                    }
+                }
+            });
 
             ViewModel = function () {
                 var self = this, model, courseModel;
@@ -30,11 +45,28 @@ define(['jquery',
                 self.comments = kb.observable(model, 'comments');
 
                 self.actualUsers = kb.observable(model, 'actualUsers');
+
                 self.missedUsers = kb.observable(model, 'missedUsers');
                 self.actualInstructors = kb.observable(model, 'actualInstructors');
 
                 self.courseName = kb.observable(courseModel, 'name');
-                self.courseUsers = kb.observable(courseModel, 'users');
+                self._courseUsers = kb.observable(courseModel, 'users');
+
+                self.courseUsers = ko.computed(function () {
+                    var users = self._courseUsers();
+                    // check for presence in missedUsers AND actualUsers
+                    var actualUsersIds = _.pluck(self.actualUsers(), model.idAttribute);
+                    var missedUsersIds = _.pluck(self.missedUsers(), model.idAttribute);
+
+                    users = users.map(function (user) {
+                        if ((actualUsersIds.indexOf(user._id) !== -1) || (missedUsersIds.indexOf(user._id) !== -1)) {
+                            return undefined;
+                        }
+                        return user;
+                    });
+
+                    return _.without(users, undefined);
+                });
 
                 self.markAsPresent = function (data) {
                     var actualUsers = model.get('actualUsers');
@@ -44,10 +76,26 @@ define(['jquery',
                     model.save();
                 };
 
+                self.removeFromActualUsers = function (data) {
+                    // find by id
+                    var actualUsers = model.get('actualUsers');
+                    actualUsers = _.without(actualUsers, data);
+                    model.set('actualUsers', actualUsers);
+                    model.save();
+                };
+
                 self.markAsMissed = function (data) {
                     var missedUsers = model.get('missedUsers');
                     missedUsers = missedUsers.slice();
                     missedUsers.push(data);
+                    model.set('missedUsers', missedUsers);
+                    model.save();
+                };
+
+                self.removeFromMissedUsers = function (data) {
+                    // find by id
+                    var missedUsers = model.get('missedUsers');
+                    missedUsers = _.without(missedUsers, data);
                     model.set('missedUsers', missedUsers);
                     model.save();
                 };
@@ -85,6 +133,21 @@ define(['jquery',
                     } else {
                         callback();
                     }
+                };
+
+                self.addUser = function () {
+                    var alreadyAddedIds = _.pluck(self._courseUsers(), '_id');
+                    view.trigger('modal', {
+                        content: _.compact(usersCollection.map(function (item) {
+                            if (alreadyAddedIds.indexOf(item.get('_id')) === -1) {
+                                return {value: item.get('_id'), label: item.get('firstName') + ' ' + item.get('lastName')};
+                            }
+                        })),
+                        type: 'list',
+                        title: 'modal.selectUsers',
+                        field: 'courseUsers',
+                        filtering: true
+                    });
                 };
             };
             this.viewModel = new ViewModel();
